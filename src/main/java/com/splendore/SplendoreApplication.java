e82314c802248a359440e5ec0420d7c2ad88b349
hanniel09 <hannielvieira1227@gmail.com>
2024-07-24 13:48:34 -0300

    initial commit

---
+++ src/main/java/com/splendore/SplendoreApplication.java
@@ -0,0 +1,13 @@
+package com.splendore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplendoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplendoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/splendore/SplendoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splendore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplendoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
