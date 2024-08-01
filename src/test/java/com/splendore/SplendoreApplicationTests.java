c6f9cf9e4ef72e0a05605810a3bfb416cb12d4ae
hanniel09 <hannielvieira1227@gmail.com>
2024-07-31 23:27:39 -0300

    config: main class tests

--- src/test/java/com/splendore/SplendoreApplicationTests.java
+++ src/test/java/com/splendore/SplendoreApplicationTests.java
@@ -1,13 +1,22 @@
 package com.splendore;
 
+import com.splendore.services.RoomsService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SplendoreApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private RoomsService roomsService;
 
+    @Test
+    void contextLoads() throws Exception {
+        assertThat(roomsService).isNotNull();
+    }
 }
