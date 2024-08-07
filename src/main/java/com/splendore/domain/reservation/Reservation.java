f3336610bd4cd7ea035b14fcf002803a8bef01ce
hanniel09 <hannielvieira1227@gmail.com>
2024-08-07 16:16:04 -0300

    feat: create Reservation domain

--- src/main/java/com/splendore/domain/reservation/Reservation.java
+++ src/main/java/com/splendore/domain/reservation/Reservation.java
@@ -1,12 +1,14 @@
 package com.splendore.domain.reservation;
 
-import com.splendore.domain.clients.Client;
+import com.splendore.domain.client.Client;
+import com.splendore.domain.rooms.Rooms;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -21,8 +23,21 @@ public class Reservation {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-
     @ManyToOne
     @JoinColumn(name = "client_id")
     private Client client;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", referencedColumnName = "id")
+    private Rooms room;
+
+    private LocalDateTime checkInDate;
+    private LocalDateTime checkOutDate;
+
+    public Reservation(Client client, Rooms room, LocalDateTime checkInDate, LocalDateTime checkOutDate) {
+        this.client = client;
+        this.room = room;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
 }
