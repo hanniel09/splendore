150c2cb718fa24b25421f331a3c7f8e71a2187ad
hanniel09 <hannielvieira1227@gmail.com>
2024-08-07 16:17:08 -0300

    feat: turn the exception more comprehensive

--- src/main/java/com/splendore/exceptions/RoomNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.splendore.exceptions;
-
-public class RoomNotFoundException extends RuntimeException {
-    public RoomNotFoundException(String message) {
-        super(message);
-    }
-}
