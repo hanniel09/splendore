07fbe548f1c0ccfe7fa3d37935e256ec72e87650
hanniel09 <hannielvieira1227@gmail.com>
2024-08-07 22:50:08 -0300

    refact: add new logic to ClientRequestDTO

--- src/main/java/com/splendore/domain/client/ClientRequestDTO.java
+++ src/main/java/com/splendore/domain/client/ClientRequestDTO.java
@@ -2,6 +2,8 @@ package com.splendore.domain.client;
 
 import com.splendore.domain.address.AddressDTO;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,8 +14,16 @@ public record ClientRequestDTO(
         String phone,
         String cpf,
         String rg,
+        AddressDTO address,
         List<UUID> reservationIds,
-        List<UUID> roomIds,
-        AddressDTO address
+        List<Long> roomIds
 ) {
+    public ClientRequestDTO {
+        if (roomIds == null) {
+            roomIds = Collections.emptyList();
+        }
+        if (reservationIds == null) {
+            reservationIds = Collections.emptyList();
+        }
+    }
 }
