2da5ee149f767882eb6138a179891eb25b6f2317
hanniel09 <hannielvieira1227@gmail.com>
2024-08-18 00:10:59 -0300

    feat: create Client Restaurant

---
+++ src/main/java/com/splendore/domain/restaurant/ClientRestaurant.java
@@ -0,0 +1,40 @@
+package com.splendore.domain.restaurant;
+
+import com.splendore.domain.restaurant.enums.ReservationStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientRestaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+    private String lastName;
+
+    private Integer tableNumber;
+    private Float price;
+    private Integer quantity;
+
+    private String email;
+    private String phone;
+    private LocalDateTime reservationDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private ReservationStatus status;
+    private String specialRequest;
+    private Float totalOrderPrice;
+}
