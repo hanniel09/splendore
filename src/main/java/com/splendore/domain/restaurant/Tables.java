8b303413be58319aaed40308a7968afd9dbdf99f
hanniel09 <hannielvieira1227@gmail.com>
2024-08-19 23:47:50 -0300

    feat: create Tables

---
+++ src/main/java/com/splendore/domain/restaurant/Tables.java
@@ -0,0 +1,29 @@
+package com.splendore.domain.restaurant;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tables {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Integer tableNumber;
+    private Integer capacity;
+    private Boolean isOccupied = false;
+
+    @OneToMany(mappedBy = "tables")
+    private List<Order> orders;
+}
