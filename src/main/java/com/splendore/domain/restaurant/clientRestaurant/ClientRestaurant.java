7945bb6dbbf2f98001c5df1eab9376e2d07961ca
hanniel09 <hannielvieira1227@gmail.com>
2024-09-27 15:18:32 -0300

    feat: create Menu entity

--- src/main/java/com/splendore/domain/restaurant/clientRestaurant/ClientRestaurant.java
+++ src/main/java/com/splendore/domain/restaurant/clientRestaurant/ClientRestaurant.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Tables
+@Table
 @Setter
 @Getter
 @AllArgsConstructor
--- src/main/java/com/splendore/domain/restaurant/menu/Menu.java
+++ src/main/java/com/splendore/domain/restaurant/menu/Menu.java
@@ -1,4 +1,35 @@
 package com.splendore.domain.restaurant.menu;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private Float price;
+
+    @ElementCollection
+    private List<String> ingredients = new ArrayList<>();
+
+    private String description;
+
+
 }
+    
