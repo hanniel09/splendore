c2e11e58b0bf6774c186444e6ce36c560f46c7f3
hanniel09 <hannielvieira1227@gmail.com>
2024-10-18 17:36:09 -0300

    feat: create TablesService

--- src/main/java/com/splendore/services/restaurant/TablesService.java
+++ src/main/java/com/splendore/services/restaurant/TablesService.java
@@ -1,7 +1,50 @@
 package com.splendore.services.restaurant;
 
+import com.splendore.domain.restaurant.tables.Tables;
+import com.splendore.domain.restaurant.tables.TablesRequestDTO;
+import com.splendore.exceptions.NotFoundException;
+import com.splendore.repositories.restaurant.TablesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TablesService {
+
+    @Autowired
+    private TablesRepository tablesRepository;
+
+    public List<Tables> getTablesRepository() {
+        return tablesRepository.findAll();
+    }
+
+    public Tables getTablesRepositoryById(Long id) {
+        return tablesRepository.findById(id).orElseThrow(()-> new NotFoundException("Table not found"));
+    }
+
+    public Tables crateTables(TablesRequestDTO tablesRequestDTO) {
+        Tables tables = new Tables();
+        tables.setTableNumber(tablesRequestDTO.tableNumber());
+        tables.setCapacity(tablesRequestDTO.capacity());
+        tables.setIsOccupied(tablesRequestDTO.isOccupied());
+        tables.setOrders(tablesRequestDTO.orders());
+        return tablesRepository.save(tables);
+    }
+
+    public Tables updateTables(Long id,TablesRequestDTO tablesRequestDTO) {
+        Tables tables = getTablesRepositoryById(id);
+        tables.setTableNumber(tablesRequestDTO.tableNumber());
+        tables.setCapacity(tablesRequestDTO.capacity());
+        tables.setIsOccupied(tablesRequestDTO.isOccupied());
+        tables.setOrders(tablesRequestDTO.orders());
+        return tablesRepository.save(tables);
+    }
+
+    public void deleteTables(Long id) {
+        Tables tables = getTablesRepositoryById(id);
+        tablesRepository.delete(tables);
+    }
+
 }
