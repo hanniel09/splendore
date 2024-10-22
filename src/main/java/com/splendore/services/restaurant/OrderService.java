2eaf0a9c47a839b8d0ff9be635847dc4c0710f4a
hanniel09 <hannielvieira1227@gmail.com>
2024-10-22 19:24:36 -0300

    feat: update OrderService

--- src/main/java/com/splendore/services/restaurant/OrderService.java
+++ src/main/java/com/splendore/services/restaurant/OrderService.java
@@ -1,7 +1,52 @@
 package com.splendore.services.restaurant;
 
+import com.splendore.domain.restaurant.order.Order;
+import com.splendore.domain.restaurant.order.OrderRequestDTO;
+import com.splendore.exceptions.NotFoundException;
+import com.splendore.repositories.restaurant.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Order getOrderById(UUID id) {
+        return orderRepository.findById(id).orElseThrow(() ->
+                new NotFoundException("Not found order with id: " + id)
+        );
+    }
+
+    public Order saveOrder(OrderRequestDTO orderRequestDTO) {
+        Order order = new Order();
+        order.setClient(orderRequestDTO.client());
+        order.setTable(orderRequestDTO.table());
+        order.setTotalPrice(orderRequestDTO.totalPrice());
+        order.setStatus(orderRequestDTO.status());
+        order.setOrderItems(orderRequestDTO.orderItems());
+        return orderRepository.save(order);
+    }
+
+    public Order updateOrder(UUID id, OrderRequestDTO orderRequestDTO) {
+        Order order = getOrderById(id);
+        order.setClient(orderRequestDTO.client());
+        order.setTable(orderRequestDTO.table());
+        order.setTotalPrice(orderRequestDTO.totalPrice());
+        order.setStatus(orderRequestDTO.status());
+        order.setOrderItems(orderRequestDTO.orderItems());
+        return orderRepository.save(order);
+    }
+
+    public void deleteOrder(UUID id) {
+        orderRepository.deleteById(id);
+    }
 }
