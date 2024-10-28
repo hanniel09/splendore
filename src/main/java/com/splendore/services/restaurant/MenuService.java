37a46e2715c648a578d3d39bc956f0e216996d45
hanniel09 <hannielvieira1227@gmail.com>
2024-10-28 15:01:51 -0300

    feat: create MenuService

--- src/main/java/com/splendore/services/restaurant/MenuService.java
+++ src/main/java/com/splendore/services/restaurant/MenuService.java
@@ -1,7 +1,49 @@
 package com.splendore.services.restaurant;
 
+import com.splendore.domain.restaurant.menu.Menu;
+import com.splendore.domain.restaurant.menu.MenuRequestDTO;
+import com.splendore.exceptions.NotFoundException;
+import com.splendore.repositories.restaurant.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MenuService {
+
+    @Autowired
+    private MenuRepository menuRepository;
+
+    public List<Menu> getAllMenus() {
+     return menuRepository.findAll();
+    }
+
+    public Menu getMenuById(Long id) {
+        return menuRepository.findById(id).orElseThrow(() ->
+                new NotFoundException("Menu not found with id: " + id)
+        );
+    }
+
+    public Menu createNewMenu(MenuRequestDTO menuRequestDTO) {
+        Menu menu = new Menu();
+        menu.setName(menuRequestDTO.name());
+        menu.setPrice(menuRequestDTO.price());
+        menu.setIngredients(menuRequestDTO.ingredients());
+        menu.setDescription(menuRequestDTO.description());
+        return menuRepository.save(menu);
+    }
+
+    public Menu updateMenu(Long id, MenuRequestDTO menuRequestDTO) {
+        Menu menu = getMenuById(id);
+        menu.setName(menuRequestDTO.name());
+        menu.setPrice(menuRequestDTO.price());
+        menu.setIngredients(menuRequestDTO.ingredients());
+        menu.setDescription(menuRequestDTO.description());
+        return menuRepository.save(menu);
+    }
+
+    public void deleteMenu(Long id){
+        menuRepository.deleteById(id);
+    }
 }
