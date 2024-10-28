4cab6268a1a8f17927f8436e50a939aaf2cc9998
hanniel09 <hannielvieira1227@gmail.com>
2024-10-28 16:06:46 -0300

    feat: update OrderItemRepository

--- src/main/java/com/splendore/repositories/restaurant/OrderItemRepository.java
+++ src/main/java/com/splendore/repositories/restaurant/OrderItemRepository.java
@@ -3,5 +3,5 @@ package com.splendore.repositories.restaurant;
 import com.splendore.domain.restaurant.order.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
