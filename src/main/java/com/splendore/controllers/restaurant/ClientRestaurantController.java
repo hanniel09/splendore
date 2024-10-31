dd67c5e961b8b5f92490e40fcbe5cff1583d8b2e
hanniel09 <hannielvieira1227@gmail.com>
2024-10-30 23:26:38 -0300

    feat: update ClientRestaurantController

--- src/main/java/com/splendore/controllers/restaurant/ClientRestaurantController.java
+++ src/main/java/com/splendore/controllers/restaurant/ClientRestaurantController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@RestController
+@RestController("/client-restaurant")
 public class ClientRestaurantController {
 
     @Autowired
