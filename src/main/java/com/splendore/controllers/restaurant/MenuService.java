3fd8d39bae34445e42200b69fcf6ce85da1fd574
hanniel09 <hannielvieira1227@gmail.com>
2024-10-30 23:27:48 -0300

    feat: create MenuService

---
+++ src/main/java/com/splendore/controllers/restaurant/MenuService.java
@@ -0,0 +1,8 @@
+package com.splendore.controllers.restaurant;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/Menu")
+public class MenuService {
+}
