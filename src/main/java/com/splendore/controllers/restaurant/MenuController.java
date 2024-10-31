e8f0625af25880977a34bb8e08771532d5aca296
hanniel09 <hannielvieira1227@gmail.com>
2024-10-30 23:28:23 -0300

    fix: rename MenuController

--- src/main/java/com/splendore/controllers/restaurant/MenuController.java
+++ src/main/java/com/splendore/controllers/restaurant/MenuController.java
@@ -1,8 +1,7 @@
 package com.splendore.controllers.restaurant;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/Menu")
-public class MenuService {
+public class MenuController {
 }
