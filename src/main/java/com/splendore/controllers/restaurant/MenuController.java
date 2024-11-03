655f452295df8f9e2b092d235f0f5f104ac6565e
hanniel09 <hannielvieira1227@gmail.com>
2024-11-02 22:01:38 -0300

    feat: update MenuController

--- src/main/java/com/splendore/controllers/restaurant/MenuController.java
+++ src/main/java/com/splendore/controllers/restaurant/MenuController.java
@@ -1,7 +1,47 @@
 package com.splendore.controllers.restaurant;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.splendore.domain.restaurant.menu.Menu;
+import com.splendore.domain.restaurant.menu.MenuRequestDTO;
+import com.splendore.services.restaurant.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController("/Menu")
 public class MenuController {
+
+    @Autowired
+    private MenuService menuService;
+
+    @GetMapping()
+    public ResponseEntity<List<Menu>> getAllMenuItems() {
+        List<Menu> menus = menuService.getAllMenus();
+        return ResponseEntity.ok(menus);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Menu> getMenuItem(@PathVariable long id) {
+        Menu menu = menuService.getMenuById(id);
+        return ResponseEntity.ok(menu);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Menu> createMenuItem(@RequestBody MenuRequestDTO menuRequestDTO) {
+     Menu menu = menuService.createNewMenu(menuRequestDTO);
+     return ResponseEntity.ok(menu);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Menu> updateMenuItem(@PathVariable long id, @RequestBody MenuRequestDTO menuRequestDTO) {
+        Menu menu = menuService.updateMenu(id, menuRequestDTO);
+        return ResponseEntity.ok(menu);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMenuItem(@PathVariable long id) {
+        menuService.deleteMenu(id);
+        return ResponseEntity.noContent().build();
+    }
 }
